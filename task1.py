14ab51fcfdb60398f0984424bf6d1b1e33724121
boburjon <128900137+jumayevv@users.noreply.github.com>
2024-03-05 23:02:38 +0500

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,6 +1,4 @@
-# fibonacci
-lst = [1,1]
-n = 5
-for i in range(n):
-    lst.append(sum(lst[i:]))
+n = int(input("enter size of fibonacci numbers: "))
+lst = [1, 1]
+[lst.append(sum(lst[i:])) for i in range(n-2)] # we decreased n because we already has two number which is fibonacci number starts with
 print(lst)
